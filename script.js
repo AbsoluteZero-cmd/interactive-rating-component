11c975540ce13f3143833660c9907e667d1f9e0d
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2022-06-20 13:44:58 +0600

    added thank you section & js for new section

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let buttons = document.querySelectorAll('.feedback__btn');
 
 let prev = null;
+let number;
 
 for (let button of buttons) {
     button.addEventListener('click', function handleClick() {
         if(button.classList.contains('chosen')){
-            button.classList.remove('chosen');
+            // button.classList.remove('chosen');
         }
         else{
             if(prev != null){
@@ -13,6 +14,15 @@ for (let button of buttons) {
             }
             button.classList.add('chosen');
             prev = button;
+            number = prev.textContent;
+            console.log(number);
         }
     });
-  }
+}
+
+let submit = document.querySelector('.feedback__submit');
+let rating = document.querySelector('.final__rating');
+submit.addEventListener('click', function handleClick() {
+    document.querySelector('.feedback').style.display = 'none';
+    rating.textContent = number;
+});
